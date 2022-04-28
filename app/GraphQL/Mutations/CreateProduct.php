f2b80801ac768cbb189ad45e07f1248a1d845cf6
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-04-28 14:20:39 +0400

    Product CRUD

---
+++ app/GraphQL/Mutations/CreateProduct.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Validation\Factory;
+
+final class CreateProduct
+{
+    public $validation;
+    public function __construct(Factory $validation)
+    {
+        $this->validation = $validation;
+    }
+
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args): Product
+    {
+        $this->validation->validate($args,[
+            'name' => 'min:3|max:50',
+            'description' => 'min:5|max:500'
+        ]);
+
+        $product = new Product();
+        $product->create([
+            'name' => $args['name'],
+            'description' => $args['description']
+        ]);
+        $category = Category::find($args['categories']);
+        $product->categories()->attach($category);
+        return $product;
+    }
+}
